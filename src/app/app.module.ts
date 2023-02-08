9c853b6791c50af60cf06f047a39d8dd4cda8b31
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2023-02-08 15:23:15 +0000

    addition to skeleton
    Signed-off-by: Musah Ibrahim Ali <musahibrahimali@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import configuration from "@common";
+import * as Joi from 'joi';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      envFilePath: [".env.local", ".env.production"],
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(5000),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    // connect to mongodb database
+    MongooseModule.forRoot("mongodb://localhost/dynasty", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/config/configuration.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 5000,
+  domain: process.env.DOMAIN,
+  database: {
+    host: process.env.MONGO_URI,
+  },
+  origin: process.env.ORIGIN_URL,
+  uploadsDir: process.env.UPLOADS_DIR,
+});
+
